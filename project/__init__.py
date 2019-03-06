ca0c778ecc960606cc03716641b8fb2689422806
swapnil <smeshram258@gmail.com>
2019-03-07 01:39:11 +0530

    updated configuration for db

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 import os
 import sys 
 
@@ -8,6 +9,21 @@ app = Flask(__name__)
 # set configuration
 app_settings = os.getenv("APP_SETTINGS")
 app.config.from_object(app_settings) # new
+
+# creae new sql database instance
+db = SQLAlchemy(app) 
+
+# first databse user model
+class User(db.Model):  # new
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.String(128), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 
 @app.route("/user/ping", methods=["GET"])
--- project/config.py
+++ project/config.py
@@ -1,16 +1,19 @@
-
+import os
 class BaseConfig:
     """Base Configuration"""
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
 
 class DevelopmentConfig(BaseConfig):
     "Development Configuration"
-    pass
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
 
 class TestingConfig(BaseConfig):
     """ Testing Configuration """
     TESTING = True
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_TEST_URL")
+
 
 class ProductionConfiguration(BaseConfig):
     """ Production Configuration """
-    pass 
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL") 
